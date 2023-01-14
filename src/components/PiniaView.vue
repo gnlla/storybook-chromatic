d8ef7a93c4d6acfa37c1e193afd46a75cd96812e
gnlla <hd.40317@gmail.com>
2023-01-14 19:25:33 +0900

    [Add] Piniaを追加

---
+++ src/components/PiniaView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+
+const count = useCounterStore();
+
+const add = () => {
+  count.increment();
+};
+</script>
+<template>
+  <div>
+    {{ count.count }}
+
+    <button @click="add">increment</button>
+  </div>
+</template>
